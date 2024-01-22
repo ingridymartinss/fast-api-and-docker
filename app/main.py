ebb927909ddb9a816b2bb55496ad3d4f8f801175
ingridymartinss <89396112+ingridymartinss@users.noreply.github.com>
2024-01-22 18:34:19 -0300

    Files

---
+++ app/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+import random
+from faker import Faker
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/users")
+async def users():
+    fake = Faker()
+    num_rows = 1000
+    users = []
+    
+    for i in range(num_rows):
+        name = fake.name()
+        city = fake.city()
+        age = random.randint(18, 99)
+        if users:
+            users.append({'name': name, 'city': city, 'age': age})
+        else:
+            users = [{'name': name, 'city': city, 'age': age}]
+    return users
